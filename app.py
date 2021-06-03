eaa9de93868bf9206e8ed029947aec0caf9668a6
bouslougui <83832100+bouslougui@users.noreply.github.com>
2021-06-03 19:50:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ IMAGE_SIZE = (150, 150)
 UPLOAD_FOLDER = 'uploads'
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+
 app = Flask(__name__, template_folder='Templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -17,6 +23,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def template_test():
     return render_template('home.html', label='', imagesource='file://null')
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            output = predict(file_path)
+    return render_template("home.html", label=output, imagesource=file_path)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+       
 
 
 if __name__ == "__main__":
